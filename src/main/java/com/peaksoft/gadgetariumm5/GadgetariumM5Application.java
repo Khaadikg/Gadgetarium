a855b469e89da7c4a21794698c9d34a6e6465e89
PC <tairovasan11@gmail.com>
2023-06-15 20:24:08 +0300

    add swagger documentation

--- src/main/java/com/peaksoft/gadgetariumm5/GadgetariumM5Application.java
+++ src/main/java/com/peaksoft/gadgetariumm5/GadgetariumM5Application.java
@@ -1,5 +1,7 @@
 package com.peaksoft.gadgetariumm5;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
+@Tag(name = "Hoho",description = "hehe")
 public class GadgetariumM5Application {
 
     public static void main(String[] args) {
@@ -14,6 +17,7 @@ public class GadgetariumM5Application {
         System.out.println("Welcome colleagues, project name is Gadgetarium-M5!");
     }
     @GetMapping("/")
+    @Operation(summary = "Welcome",description = "Welcome to Gadgetarium-M5 Application!!!")
     public String greetingPage() {
         return "<h1>Welcome to Gadgetarium-M5 Application!!!<h1/>";
     }
