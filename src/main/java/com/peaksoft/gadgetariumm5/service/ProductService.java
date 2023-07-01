a3abf2b49eae2a4f8db3dc412d51b99b77b04b2b
bakytlatibov <bakytlatibov3@gmail.com>
2023-07-01 15:30:11 +0300

    search product

--- src/main/java/com/peaksoft/gadgetariumm5/service/ProductService.java
+++ src/main/java/com/peaksoft/gadgetariumm5/service/ProductService.java
@@ -2,13 +2,17 @@ package com.peaksoft.gadgetariumm5.service;
 
 import com.peaksoft.gadgetariumm5.dto.ProductResponse;
 import com.peaksoft.gadgetariumm5.dto.ProductResponseVIew;
-import com.peaksoft.gadgetariumm5.model.entity.Product;
+import com.peaksoft.gadgetariumm5.model.entity.*;
+import com.peaksoft.gadgetariumm5.model.enums.*;
 import com.peaksoft.gadgetariumm5.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.*;
+import java.io.File;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,13 +24,28 @@ public class ProductService {
 
     public ProductResponse mapToResponse(Product product) {
         ProductResponse productResponse = new ProductResponse();
+
         productResponse.setBrand(product.getBrand());
+        productResponse.setName(product.getName());
+        productResponse.setArticle(product.getArticle());
+        productResponse.setWarranty(product.getWarranty());
         productResponse.setColor(product.getColor());
         productResponse.setMemory(product.getMemory());
         productResponse.setScreen(product.getScreen());
-        productResponse.setScreen(product.getScreen());
+        productResponse.setPrice(product.getPrice());
+        productResponse.setInStock(product.getInStock());
+        productResponse.setRating(product.getRating());
+        productResponse.setReleaseDate(product.getReleaseDate());
+        productResponse.setBodyShape(product.getBodyShape());
+        productResponse.setOperatingSystem(product.getOperatingSystem());
+        productResponse.setSimCard(product.getSimCard());
+        productResponse.setProcessor(product.getProcessor());
+        productResponse.setWeight(product.getWeight());
+        productResponse.setGender(product.getGender());
+        productResponse.setWaterResistance(product.getWaterResistance());
+        productResponse.setWirelessInterface(product.getWirelessInterface());
+        productResponse.setDiscount(product.getDiscount());
         return productResponse;
-
     }
 
     public ProductResponseVIew searchAndPagination(String text, int page, int size) {
